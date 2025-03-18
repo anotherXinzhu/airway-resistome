efb2b04b077810d94844f35cf2b83ad03a6c25d6
anotherXinzhu <labxz@hotmail.com>
2025-03-18 19:18:13 +0800

    Update linearRegression.r

--- Figure2/linearRegression.r
+++ Figure2/linearRegression.r
@@ -31,15 +31,14 @@ for(arg.v in colnames(ARG.dat)[-1]){
     
     meta_c <- meta.st %>% select(SampleID,all_of(v))
     
-    # 先去掉NA ----
+    # remove NA ----
     meta_c <- meta_c[complete.cases(meta_c),]
     if(length(unique(meta_c[,2])) == 1) next
     
-    # 统一X和Y排序----
+    # match samples ---
     ARG_c <- ARG.dat %>% select(Sample, all_of(arg.v))
     ARG_c <- ARG_c[match(meta_c$SampleID, ARG_c$Sample),]
     
-    # statistics: lm without Covars ---------------
     testD <- cbind.data.frame(
       meta_c,
       ARGvalue = ARG_c[,2],
@@ -47,7 +46,7 @@ for(arg.v in colnames(ARG.dat)[-1]){
     ) %>% relocate(ARGvalue, .after = 'SampleID')
     
     
-    # Lm with covars ---------------
+    # Linear regression ---------------
     testD.covars <- merge(testD, covar.df, by = "SampleID")
     s1 <- lm(as.formula(paste0("ARGvalue ~ ", paste0(covars,collapse = "+" ), "+", colnames(testD)[3])), data = testD.covars)
     s0 <- lm(as.formula(paste0("ARGvalue ~ ", paste0(covars,collapse = "+" ))), data = testD.covars)
